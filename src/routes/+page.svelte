08e5a8fc6e89dab7f862d39b8d4a487053fd5698
abdelhalim <rioman19@gmail.com>
2024-01-03 14:19:48 -0500

    use var

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,36 @@
 <script lang="ts">
 	import WalletsContainer from '$lib/WalletsContainer.svelte';
 	import PendingItem from '$lib/PendingItem.svelte';
+
+	let timeLeft = 60;
 </script>
 
 <WalletsContainer>
-	<PendingItem type="update" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
+	<PendingItem type="update" chainId={1} timeLeftSeconds={timeLeft} transactionHash="abcdef">
 		“oldName” → “newName”
 	</PendingItem>
 
-	<PendingItem type="add" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
+	<PendingItem type="add" chainId={1} timeLeftSeconds={timeLeft} transactionHash="abcdef">
 		ryeshrimp/main
 	</PendingItem>
 
-	<PendingItem type="extend" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
+	<PendingItem type="extend" chainId={1} timeLeftSeconds={timeLeft} transactionHash="abcdef">
 		{`by {durationLength}`}
 	</PendingItem>
 
-	<PendingItem type="transfer" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
+	<PendingItem type="transfer" chainId={1} timeLeftSeconds={timeLeft} transactionHash="abcdef">
 		Sending foobar/ to 0xAbCd...
 	</PendingItem>
 
-	<PendingItem type="remove" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
+	<PendingItem type="remove" chainId={1} timeLeftSeconds={timeLeft} transactionHash="abcdef">
 		You will no longer own foobar/
 	</PendingItem>
 
-	<PendingItem type="buy" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
+	<PendingItem type="buy" chainId={1} timeLeftSeconds={timeLeft} transactionHash="abcdef">
 		Registered “...”
 	</PendingItem>
 
-	<PendingItem type="createBid" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
+	<PendingItem type="createBid" chainId={1} timeLeftSeconds={timeLeft} transactionHash="abcdef">
 		3.9 ETH for “...”
 	</PendingItem>
 </WalletsContainer>
