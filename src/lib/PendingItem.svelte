34617452fc20aae1f7430536f79b1e194d999f65
abdelhalim <rioman19@gmail.com>
2024-01-03 14:10:05 -0500

    adjust spacings

--- src/lib/PendingItem.svelte
+++ src/lib/PendingItem.svelte
@@ -19,25 +19,18 @@
 <script lang="ts">
 	import dayjs from 'dayjs';
 	import { ExtendIcon, ExternalIcon } from './icons';
-	import { nowStore } from '$lib/stores';
 	import { derived } from 'svelte/store';
 
 	export let type: Type;
 	export let chainId: number;
 	export let timeLeftSeconds: number;
 	export let transactionHash: string;
-
-	// Create dayjs instance after `timeLeftSeconds` from now
-	const timeLeftAt = dayjs().add(timeLeftSeconds, 'seconds');
-
-	// Calculate reactivly the time left
-	const timeLeftStore = derived(nowStore, (now) => Math.max(0, timeLeftAt.diff(now, 'seconds')), 0);
 </script>
 
 <div class="pending-item">
 	<div class="flex gap-3 col-span-full md:col-span-1 md:flex-1">
 		<button
-			class="w-11 h-11 bg-neutral-100 border border-neutral-300 rounded-lg flex items-center justify-center text-xl shrink-0 dark:bg-neutral-900 dark:border-neutral-700"
+			class="w-[41px] h-[43px] bg-neutral-100 border border-neutral-300 rounded-[10px] flex items-center justify-center text-xl shrink-0 dark:bg-neutral-900 dark:border-neutral-700"
 		>
 			{#await items[type] then comp}
 				<svelte:component this={comp.default}></svelte:component>
@@ -55,7 +48,7 @@
 	<div class="flex md:flex-col items-center col-span-2 md:col-span-1 md:items-end">
 		<div class="flex items-center gap-1">
 			<img
-				class="w-4 h-4 bg-neutral-300 rounded-full text-2xl leading-[1]"
+				class="w-[17px] h-[17px] bg-neutral-300 rounded-full text-2xl leading-[1]"
 				src="/images/chain/{chainId}.jpg"
 				alt="currency icon"
 			/>
@@ -65,16 +58,18 @@
 
 		<div class="flex-1 md:hidden"></div>
 
-		<div class="flex gap-1 items-center opacity-50 truncate text-small">
-			<div class="text-base align-middle flex justify-center leading-[1]"><ExtendIcon /></div>
-			<div class="font-medium truncate">{$timeLeftStore}s left</div>
+		<div class="flex gap-1 items-center truncate text-small">
+			<div class="text-base leading-[1] opacity-30">
+				<ExtendIcon />
+			</div>
+			<div class="font-medium truncate opacity-50">{timeLeftSeconds}s left</div>
 		</div>
 	</div>
 
 	<div class="hidden md:block border-l border-neutral-300 h-full"></div>
 
 	<a
-		class="w-10 h-10 flex items-center justify-center border border-neutral-300 dark:border-neutral-700 rounded-md text-xl leading-[1]"
+		class="w-[30px] h-[30px] md:w-10 md:h-10 flex items-center justify-center border border-neutral-300 dark:border-neutral-700 rounded-md md:rounded-[10px] text-base md:text-xl leading-[1]"
 		href="http://test.com/{transactionHash}"
 	>
 		<ExternalIcon />
@@ -83,12 +78,14 @@
 
 <style lang="postcss">
 	.pending-item {
-		@apply grid gap-6 border border-neutral-200 rounded-lg p-3 items-center bg-white dark:bg-neutral-800 dark:border-neutral-700 text-black dark:text-white;
+		@apply grid border border-neutral-200 rounded-lg py-3 px-4 items-center bg-white dark:bg-neutral-800 dark:border-neutral-700 text-black dark:text-white;
 
 		grid-template-columns: 1fr auto auto;
+		gap: theme('spacing.5');
 
 		@media (min-width: theme('screens.md')) {
 			grid-template-columns: 1fr auto auto auto;
+			gap: theme('spacing.4');
 		}
 	}
 </style>
