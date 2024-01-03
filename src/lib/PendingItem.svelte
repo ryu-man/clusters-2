8ca57af9b2f185d525c96049f882334859f75bad
abdelhalim <rioman19@gmail.com>
2024-01-03 13:23:35 -0500

    add anchor tag

--- src/lib/PendingItem.svelte
+++ src/lib/PendingItem.svelte
@@ -73,11 +73,12 @@
 
 	<div class="hidden md:block border-l border-neutral-300 h-full"></div>
 
-	<button
+	<a
 		class="w-10 h-10 flex items-center justify-center border border-neutral-300 rounded-md text-xl leading-[1]"
+		href="http://test.com/{transactionHash}"
 	>
 		<ExternalIcon />
-	</button>
+	</a>
 </div>
 
 <style lang="postcss">
