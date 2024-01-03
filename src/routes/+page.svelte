0c8edae00f35a46bd702717200c995dbf9da2665
abdelhalim <rioman19@gmail.com>
2024-01-03 13:24:08 -0500

    update props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,31 +4,31 @@
 </script>
 
 <WalletsContainer>
-	<PendingItem type="update" chainId={1} timeLeftSeconds={60} transactionHash="http://test.com/">
+	<PendingItem type="update" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
 		“oldName” → “newName”
 	</PendingItem>
 
-	<PendingItem type="add" chainId={1} timeLeftSeconds={60} transactionHash="http://test.com/">
+	<PendingItem type="add" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
 		ryeshrimp/main
 	</PendingItem>
 
-	<PendingItem type="extend" chainId={1} timeLeftSeconds={60} transactionHash="http://test.com/">
+	<PendingItem type="extend" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
 		{`by {durationLength}`}
 	</PendingItem>
 
-	<PendingItem type="transfer" chainId={1} timeLeftSeconds={60} transactionHash="http://test.com/">
+	<PendingItem type="transfer" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
 		Sending foobar/ to 0xAbCd...
 	</PendingItem>
 
-	<PendingItem type="remove" chainId={1} timeLeftSeconds={60} transactionHash="http://test.com/">
+	<PendingItem type="remove" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
 		You will no longer own foobar/
 	</PendingItem>
 
-	<PendingItem type="buy" chainId={1} timeLeftSeconds={60} transactionHash="http://test.com/">
+	<PendingItem type="buy" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
 		Registered “...”
 	</PendingItem>
 
-	<PendingItem type="createBid" chainId={1} timeLeftSeconds={60} transactionHash="http://test.com/">
+	<PendingItem type="createBid" chainId={1} timeLeftSeconds={60} transactionHash="abcdef">
 		3.9 ETH for “...”
 	</PendingItem>
 </WalletsContainer>
